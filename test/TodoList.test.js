eb5e708acec883cd0f69b5ac46620c0024107c21
Hemant <hemant.pardeshi17@vit.edu>
2019-12-28 22:52:05 +0530

    Connected client app and blockchian

---
+++ test/TodoList.test.js
@@ -0,0 +1,24 @@
+const TodoList = artifacts.require('./TodoList.sol')
+
+contract('TodoList', (accounts) => {
+	before(async ()=>{
+		this.todoList = await TodoList.deployed()
+	})
+	
+	it('deploys successfully', async ()=>{
+		const address = await TodoList.address
+		assert.notEqual(address, 0x0)
+		assert.notEqual(address, '')
+		assert.notEqual(address, null)
+		assert.notEqual(address, undefined)
+	})
+	
+	it('lists task' , async ()=>{
+		const taskCount = await this.todoList.taskCount()
+		const task = await this.todoList.tasks(taskCount)
+		assert.equal(task.id.toNumber(),taskCount.toNumber())
+		assert.equal(task.id.toNumber(),1)
+		assert.equal(task.content,'Default Task')
+		assert.equal(task.completed,false)
+	})
+})
